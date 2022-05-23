0aacbd53a61ee103f09daeadc7ec4eab6210311c
Joao Guilherme Bonenti Nobrega <69513119+JoaoBonenti@users.noreply.github.com>
2022-05-23 17:23:43 -0300

    Update CensoDemog.java

--- CensoDemog.java
+++ CensoDemog.java
@@ -3,41 +3,43 @@ public class CensoDemog {
     public static void main(String[] args){
         //Uso do Scanner para entrada de dados
         Scanner scan = new Scanner(System.in);
-		//Declaração das variáveis 
+	//Declaração das variáveis 
         int i, genero, igr=0, idade=0;
-		double outros=0; 
-		float altura, midh=0, maml=0, migp=0, mediaIDespec, contMidh=0, contMaml=0, contGP=0, contOutros=0, contIdade=0, contIDespec=0;
+	double outros=0; 
+	float altura, midh=0, maml=0, migp=0, mediaIDespec, contMidh=0, contMaml=0, contGP=0, contOutros=0, contIdade=0, contIDespec=0;
 		
         //Utilização da estrutura de repetição For
         for (i=0; i < 1000; i++) {
 		
-		    //Entrada de dados da variável genero
+	    //Entrada de dados da variável genero
             System.out.println("Digite '0' se for do genero feminino, '1' para masculino e '2' para outros: ");
-		    genero = scan.nextInt();
-		    contGP++;
+	    genero = scan.nextInt();
+	    //Contador
+	    contGP++;
             //Entrada de dados da variável idade
             System.out.println("Qual e a sua idade? ");
-		    idade = scan.nextInt();
+	    idade = scan.nextInt();
             contIdade++;
-		    if (idade >= 18 && idade <= 35) {
+	    //Condicional para determinar se o usuário se encaixa no grupo especificado
+            if (idade >= 18 && idade <= 35) {
             contIDespec++;
             }
             //Função que realiza a soma da variável idade 
             igr = igr + idade;
-	        //Entrada de dados da variável altura
+	    //Entrada de dados da variável altura
             System.out.println("Qual e a sua altura? ");
-		    altura = scan.nextFloat();
+            altura = scan.nextFloat();
 		
-		    //Condicional para identificação de gêneros e separação de informações pertinentes
+	    //Condicional para identificação de gêneros e separação de informações pertinentes
             if ( genero==1) {
 			midh = midh + idade;
 			contMidh++;
 		    }
-		    else if (genero==0) {
+	    else if (genero==0) {
 			maml = maml + altura;
 			contMaml++;
 		    }
-		    else if (genero==2) {
+	    else if (genero==2) {
 			outros = outros +0;
 			contOutros++;        
             }
